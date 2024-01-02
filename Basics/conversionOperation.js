4e7e1e9aaad1d67096f832a6052bdd0ea1d39557
Abhinandan Kumar <abhinandankumar7026@gmail.com>
2024-01-02 23:11:59 +0530

    conversion of datatypes

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,14 @@
+let score="33abc";
+console.log(typeof score);
+let valueinNumber=Number(score);
+//"33"->number
+//"33abc"->NaN(a type of number)
+// Disadvantage of Javascript thats why we use typescript
+console.log(typeof NaN);
+
+let isLoggedIN="Abhi";
+let booleanIsloggedIN=Boolean(isLoggedIN);
+console.log(booleanIsloggedIN);
+//1-> true; 0->false
+//""->false;, Any value "Abhinandan"->true;
+
