750b28d55e9eabb2d198baf12cecd4c713dee488
Abhinandan Kumar <abhinandankumar7026@gmail.com>
2024-01-02 22:58:05 +0530

    datatype

---
+++ Basics/datatype.js
@@ -0,0 +1,16 @@
+let score=null;
+//console.log(score);
+console.log(typeof(score));
+let name="Abhinandan"
+let age=22
+let isLoggedIN=true;
+// number->2 to power 53
+//string->""
+//boolean->true/false
+//null->stand alone value
+//undefined->not defined
+//symbol->uniqueness
+
+// object
+
+
