5acc0af770eb99c5a5ac60c39eef03c6c6d4aa0f
Abhinandan Kumar <abhinandankumar7026@gmail.com>
2024-01-04 18:02:15 +0530

    updated

--- Basics/01string.js
+++ Basics/01string.js
@@ -1,3 +1,25 @@
 const name="Abhinandan"
 const repocount=100
-console.log(name + repocount +"Value");
+//console.log(name + repocount +"Value");
+
+//NEw Method
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+ const gamename=new String('Abhinandan-Ab');
+ console.log(gamename.__proto__);
+ console.log(gamename.length);
+ console.log(gamename.charAt(9));
+ console.log(gamename.indexOf('i'));
+
+
+ const newString=gamename.substring(0,4);
+ console.log(newString);
+ // split
+ //trim
+ //read all this methods of string in mdn or official documentaion
+ 
+
+
+
+
+
---
+++ Basics/numAndmath.js
@@ -0,0 +1,13 @@
+const scor=100
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber=123.2234567
+console.log(otherNumber.toPrecision(5));
+ const paisa=100000
+ console.log(paisa.toLocaleString());
+
+
