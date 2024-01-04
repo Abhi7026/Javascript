0c00ff446ec633dc90732d0f9c639068aeac5f8f
Abhinandan Kumar <abhinandankumar7026@gmail.com>
2024-01-04 23:06:34 +0530

    updated

--- Basics/numAndmath.js
+++ Basics/numAndmath.js
@@ -1,13 +1,23 @@
 const scor=100
 const balance=new Number(100)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
 
 const otherNumber=123.2234567
-console.log(otherNumber.toPrecision(5));
+//console.log(otherNumber.toPrecision(5));
  const paisa=100000
- console.log(paisa.toLocaleString());
+ //console.log(paisa.toLocaleString('en-IN');
 
+ //*****************  Maths **************
+ 
+//  console.log(Math);
+//  console.log(Math.max(9,2,3,44,5));
+ console.log((Math.random()*10)+1);
 
+ const min=10;
+ const max=20;
+ console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+ 
